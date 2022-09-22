94148c0d7d105adf5ba58c8caf6a73ba988136a7
Shin <99096179+shinthedev@users.noreply.github.com>
2022-09-22 23:02:51 +0200

    Initial commit

---
+++ .svelte-kit/runtime/env/static/private.js
@@ -0,0 +1,186 @@
+// this file is generated — do not edit it
+/** @type {import('$env/static/private').NVM_INC} */
+export const NVM_INC = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/include/node";
+
+/** @type {import('$env/static/private').MANPATH} */
+export const MANPATH = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/share/man:/opt/homebrew/share/man::";
+
+/** @type {import('$env/static/private').TERM_PROGRAM} */
+export const TERM_PROGRAM = "iTerm.app";
+
+/** @type {import('$env/static/private').NODE} */
+export const NODE = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/bin/node";
+
+/** @type {import('$env/static/private').INIT_CWD} */
+export const INIT_CWD = "/Users/mekylenaidoo/Documents/GitHub/Shin-Starter";
+
+/** @type {import('$env/static/private').NVM_CD_FLAGS} */
+export const NVM_CD_FLAGS = "-q";
+
+/** @type {import('$env/static/private').TERM} */
+export const TERM = "xterm-256color";
+
+/** @type {import('$env/static/private').SHELL} */
+export const SHELL = "/bin/zsh";
+
+/** @type {import('$env/static/private').npm_config_metrics_registry} */
+export const npm_config_metrics_registry = "https://registry.npmjs.org/";
+
+/** @type {import('$env/static/private').HOMEBREW_REPOSITORY} */
+export const HOMEBREW_REPOSITORY = "/opt/homebrew";
+
+/** @type {import('$env/static/private').TMPDIR} */
+export const TMPDIR = "/var/folders/4j/ftr7s0rx6zx7b4xnwn4whzzm0000gn/T/";
+
+/** @type {import('$env/static/private').npm_config_global_prefix} */
+export const npm_config_global_prefix = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0";
+
+/** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
+export const TERM_PROGRAM_VERSION = "3.4.16";
+
+/** @type {import('$env/static/private').COLOR} */
+export const COLOR = "1";
+
+/** @type {import('$env/static/private').TERM_SESSION_ID} */
+export const TERM_SESSION_ID = "w0t0p0:4CC94D62-C478-4520-A7BD-2311E553D83D";
+
+/** @type {import('$env/static/private').npm_config_noproxy} */
+export const npm_config_noproxy = "";
+
+/** @type {import('$env/static/private').npm_config_local_prefix} */
+export const npm_config_local_prefix = "/Users/mekylenaidoo/Documents/GitHub/Shin-Starter";
+
+/** @type {import('$env/static/private').ZSH} */
+export const ZSH = "/Users/mekylenaidoo/.oh-my-zsh";
+
+/** @type {import('$env/static/private').NVM_DIR} */
+export const NVM_DIR = "/Users/mekylenaidoo/.nvm";
+
+/** @type {import('$env/static/private').USER} */
+export const USER = "mekylenaidoo";
+
+/** @type {import('$env/static/private').COMMAND_MODE} */
+export const COMMAND_MODE = "unix2003";
+
+/** @type {import('$env/static/private').npm_config_globalconfig} */
+export const npm_config_globalconfig = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/etc/npmrc";
+
+/** @type {import('$env/static/private').SSH_AUTH_SOCK} */
+export const SSH_AUTH_SOCK = "/private/tmp/com.apple.launchd.56zFYVJ9iY/Listeners";
+
+/** @type {import('$env/static/private').__CF_USER_TEXT_ENCODING} */
+export const __CF_USER_TEXT_ENCODING = "0x1F5:0x0:0x0";
+
+/** @type {import('$env/static/private').npm_execpath} */
+export const npm_execpath = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/lib/node_modules/npm/bin/npm-cli.js";
+
+/** @type {import('$env/static/private').PAGER} */
+export const PAGER = "less";
+
+/** @type {import('$env/static/private').LSCOLORS} */
+export const LSCOLORS = "Gxfxcxdxbxegedabagacad";
+
+/** @type {import('$env/static/private').PATH} */
+export const PATH = "/Users/mekylenaidoo/Documents/GitHub/Shin-Starter/node_modules/.bin:/Users/mekylenaidoo/Documents/GitHub/node_modules/.bin:/Users/mekylenaidoo/Documents/node_modules/.bin:/Users/mekylenaidoo/node_modules/.bin:/Users/node_modules/.bin:/node_modules/.bin:/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/bin:/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin:/Users/mekylenaidoo/.cargo/bin";
+
+/** @type {import('$env/static/private').npm_package_json} */
+export const npm_package_json = "/Users/mekylenaidoo/Documents/GitHub/Shin-Starter/package.json";
+
+/** @type {import('$env/static/private')._} */
+export const _ = "/Users/mekylenaidoo/Documents/GitHub/Shin-Starter/node_modules/.bin/vite";
+
+/** @type {import('$env/static/private').npm_config_userconfig} */
+export const npm_config_userconfig = "/Users/mekylenaidoo/.npmrc";
+
+/** @type {import('$env/static/private').npm_config_init_module} */
+export const npm_config_init_module = "/Users/mekylenaidoo/.npm-init.js";
+
+/** @type {import('$env/static/private').__CFBundleIdentifier} */
+export const __CFBundleIdentifier = "com.googlecode.iterm2";
+
+/** @type {import('$env/static/private').npm_command} */
+export const npm_command = "run-script";
+
+/** @type {import('$env/static/private').PWD} */
+export const PWD = "/Users/mekylenaidoo/Documents/Github/Shin-Starter";
+
+/** @type {import('$env/static/private').npm_lifecycle_event} */
+export const npm_lifecycle_event = "dev";
+
+/** @type {import('$env/static/private').EDITOR} */
+export const EDITOR = "vi";
+
+/** @type {import('$env/static/private').npm_package_name} */
+export const npm_package_name = "sveltekit-daisyui-template";
+
+/** @type {import('$env/static/private').ITERM_PROFILE} */
+export const ITERM_PROFILE = "Default";
+
+/** @type {import('$env/static/private').XPC_FLAGS} */
+export const XPC_FLAGS = "0x0";
+
+/** @type {import('$env/static/private').npm_config_node_gyp} */
+export const npm_config_node_gyp = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
+
+/** @type {import('$env/static/private').npm_package_version} */
+export const npm_package_version = "0.0.1";
+
+/** @type {import('$env/static/private').XPC_SERVICE_NAME} */
+export const XPC_SERVICE_NAME = "0";
+
+/** @type {import('$env/static/private').SHLVL} */
+export const SHLVL = "2";
+
+/** @type {import('$env/static/private').HOME} */
+export const HOME = "/Users/mekylenaidoo";
+
+/** @type {import('$env/static/private').COLORFGBG} */
+export const COLORFGBG = "7;0";
+
+/** @type {import('$env/static/private').LC_TERMINAL_VERSION} */
+export const LC_TERMINAL_VERSION = "3.4.16";
+
+/** @type {import('$env/static/private').HOMEBREW_PREFIX} */
+export const HOMEBREW_PREFIX = "/opt/homebrew";
+
+/** @type {import('$env/static/private').ITERM_SESSION_ID} */
+export const ITERM_SESSION_ID = "w0t0p0:4CC94D62-C478-4520-A7BD-2311E553D83D";
+
+/** @type {import('$env/static/private').npm_config_cache} */
+export const npm_config_cache = "/Users/mekylenaidoo/.npm";
+
+/** @type {import('$env/static/private').LESS} */
+export const LESS = "-R";
+
+/** @type {import('$env/static/private').LOGNAME} */
+export const LOGNAME = "mekylenaidoo";
+
+/** @type {import('$env/static/private').npm_lifecycle_script} */
+export const npm_lifecycle_script = "vite dev";
+
+/** @type {import('$env/static/private').LC_CTYPE} */
+export const LC_CTYPE = "UTF-8";
+
+/** @type {import('$env/static/private').NVM_BIN} */
+export const NVM_BIN = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/bin";
+
+/** @type {import('$env/static/private').npm_config_user_agent} */
+export const npm_config_user_agent = "npm/8.17.0 node/v16.16.0 darwin arm64 workspaces/false";
+
+/** @type {import('$env/static/private').INFOPATH} */
+export const INFOPATH = "/opt/homebrew/share/info:";
+
+/** @type {import('$env/static/private').HOMEBREW_CELLAR} */
+export const HOMEBREW_CELLAR = "/opt/homebrew/Cellar";
+
+/** @type {import('$env/static/private').LC_TERMINAL} */
+export const LC_TERMINAL = "iTerm2";
+
+/** @type {import('$env/static/private').npm_node_execpath} */
+export const npm_node_execpath = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0/bin/node";
+
+/** @type {import('$env/static/private').npm_config_prefix} */
+export const npm_config_prefix = "/Users/mekylenaidoo/.nvm/versions/node/v16.16.0";
+
+/** @type {import('$env/static/private').COLORTERM} */
+export const COLORTERM = "truecolor";
---
+++ src/routes/index.svelte
@@ -0,0 +1,36 @@
+<script>
+    // import transaction build from aptos sdk: https://github.com/aptos-labs/aptos-core/tree/main/ecosystem/typescript/sdk
+import { BCS, TxnBuilderTypes } from 'aptos';
+
+// Establish connection to the wallet
+const result = await (window as any).aptos.connect()
+
+// Check connection status of wallet
+const status = await (window as any).aptos.isConnected()
+
+// Gets the address of the account signed into the wallet
+const accountAddress = await (window as any).aptos.account()
+
+// Create a transaction
+const transaction = {
+    arguments: [address, '717'],
+    function: '0x1::coin::transfer',
+    type: 'entry_function_payload',
+    type_arguments: ['0x1::aptos_coin::AptosCoin'],
+};
+
+// Send transaction to the extension to be signed and submitted to chain
+const response = await (window as any).aptos.signAndSubmitTransaction(transaction)
+
+// Send transaction to the extension to be signed and returns
+const signedTransaction = await (window as any).aptos.signTransaction(transaction)
+
+// Disconnect dApp from the wallet
+await (window as any).aptos.disconnect(transaction)
+</script>
+
+
+
+<h1 class="text-lg font-sans text-red-300 p-4 ">Hello world!</h1>
+<p class="text-md font-sans text-black p-4 pt-0">This is a basic starter for building web3 projects using svelte<br /><br />Click the button below to connect your wallet</p>
+<a class="btn ml-4">Connect wallet</a>
