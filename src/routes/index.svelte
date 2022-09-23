ce0ec2bdf3716eb942ef446b1954fa37bdc86f9a
Shin <99096179+shinthedev@users.noreply.github.com>
2022-09-23 15:55:19 +0200

    Added wallet support

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
+
     // import transaction build from aptos sdk: https://github.com/aptos-labs/aptos-core/tree/main/ecosystem/typescript/sdk
 import { BCS, TxnBuilderTypes } from 'aptos';
+
+let userAddress: String;
 
 async function connectWallet(){
 // Establish connection to the wallet
@@ -12,15 +15,14 @@ const status = await (window as any).aptos.isConnected()
 // Gets the address of the account signed into the wallet
 const accountAddress = await (window as any).aptos.account()
 
-console.log(result,status,accountAddress)
-return(accountAddress)
+userAddress = accountAddress.address;
+
+console.log(userAddress)
+
 }
 
 </script>
-
-
-
 <h1 class="text-lg font-sans text-red-300 p-4 ">Hello world!</h1>
 <p class="text-md font-sans text-black p-4 pt-0">This is a basic starter for building web3 projects using svelte<br /><br />Click the button below to connect your wallet</p>
 <a class="btn ml-4" on:click={connectWallet}>Connect wallet</a>
-<h1>{}</h1>
+<h1>{userAddress}</h1>
